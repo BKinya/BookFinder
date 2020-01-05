923b08d0789d440c34927f62981ad94bb5efc9b5
BKinya <gitongakinya1@gmail.com>
2020-01-05 08:30:46 +0300

    first navigation to the login screen

---
+++ app/src/main/java/com/beatrice/bookfinder/WelcomeFragment.java
@@ -0,0 +1,37 @@
+package com.beatrice.bookfinder;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class WelcomeFragment extends Fragment {
+
+
+    public WelcomeFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View rootView = inflater.inflate(R.layout.fragment_welcome, container, false);
+        rootView.findViewById(R.id.next_txtview).setOnClickListener(
+                Navigation.createNavigateOnClickListener(R.id.action_welcomeFragment_to_loginFragment)
+        );
+
+        return rootView;
+    }
+
+}
